6727e755c9222a8d954f92bf822f6aa850d33bb1
Stepan <stepantishhen@mail.ru>
2024-05-07 21:03:26 +0300

    add display toolmodule without selecting fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
 import Header from "../../components/header/Header.tsx";
 import List from "../../components/list/List.tsx";
 import Display from "../../components/display/Display.tsx";
 
 const HomePage: React.FC = () => {
-    const [selectedModuleId, setSelectedModuleId] = useState<string | null>(null);
-
-    const handleModuleSelect = (moduleId: string) => {
-        setSelectedModuleId(moduleId);
-    };
-
     return (
         <div className="container">
             <Header/>
-            <List onModuleSelect={handleModuleSelect}/>
-            <Display selectedModuleId={selectedModuleId} />
+            <List />
+            <Display />
         </div>
     );
 };
